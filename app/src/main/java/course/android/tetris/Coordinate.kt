39c80c64c75cbb8f4c52d4da73788c0c9445f3b0
sfruff <armanme@mail.ru>
2022-05-31 16:36:33 +0400

    add Coordinate class

---
+++ app/src/main/java/course/android/tetris/Coordinate.kt
@@ -0,0 +1,27 @@
+package course.android.tetris
+
+class Coordinate {
+    var y: Int = 0
+    var x: Int = 0
+
+    constructor(y: Int, x: Int) {
+        this.y = y
+        this.x = x
+    }
+
+    fun add(A: Coordinate, B: Coordinate):Coordinate {
+        return Coordinate(A.y + B.y, A.x + B.x)
+    }
+
+    fun sub(A: Coordinate, B: Coordinate):Coordinate {
+        return Coordinate(A.y - B.y, A.x - B.x)
+    }
+
+    fun rotateAntiClock(X: Coordinate):Coordinate {
+        return Coordinate(-X.x, X.y)
+    }
+
+    fun isEqual(A: Coordinate, B: Coordinate): Boolean {
+        return A.y == B.y && A.x == B.x
+    }
+}
