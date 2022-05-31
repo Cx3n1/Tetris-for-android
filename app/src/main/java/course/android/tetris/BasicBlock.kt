a987eed4322422aa2efbbffa6861136d3fa911fd
sfruff <armanme@mail.ru>
2022-05-31 18:27:29 +0400

    BasicBlock.kt

--- app/src/main/java/course/android/tetris/BasicBlock.kt
+++ app/src/main/java/course/android/tetris/BasicBlock.kt
@@ -1,5 +1,46 @@
 package course.android.tetris
 
-class BasicBlock {
+internal enum class BasicBlockState {
+    ON_EMPTY, ON_TETRAMINO
+}
 
+internal class BasicBlock {
+    var colour: Int
+    var tetraId: Int
+    var coordinate: Coordinate
+    var state: BasicBlockState
+
+    constructor(row: Int, column: Int) {
+        colour = -1
+        tetraId = -1
+        coordinate = Coordinate(row, column)
+        state = BasicBlockState.ON_EMPTY
+    }
+
+    constructor(colour: Int, tetraId: Int, coordinate: Coordinate, state: BasicBlockState) {
+        this.colour = colour
+        this.tetraId = tetraId
+        this.coordinate = coordinate
+        this.state = state
+    }
+
+    fun copy(): BasicBlock {
+        return BasicBlock(colour, tetraId, coordinate, state)
+    }
+
+    fun set(B: BasicBlock) {
+        colour = B.colour
+        tetraId = B.tetraId
+        coordinate.y = B.coordinate.y
+        coordinate.x = B.coordinate.x
+        state = B.state
+    }
+
+    fun setEmptyBlock(coordinate: Coordinate) {
+        colour = -1
+        tetraId = -1
+        this.coordinate.x = coordinate.x
+        this.coordinate.y = coordinate.y
+        state = BasicBlockState.ON_EMPTY
+    }
 }
